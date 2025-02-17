fb9d0c1c71c2d7a6ab9b6464dd90119608723a84
kyledavidd <davidyhuankyled@gmail.com>
2025-02-17 19:53:38 +0800

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,111 @@
+import { useState } from 'react';
+import { goatList } from './data';
+import Box from '@mui/material/Box';
+import Container from '@mui/material/Container';
+import Stack from '@mui/material/Stack';
+import Button from '@mui/material/Button';
+import IconButton from '@mui/material/IconButton';
+import KeyboardArrowDownIcon from '@mui/icons-material/KeyboardArrowDown';
+import KeyboardArrowUpIcon from '@mui/icons-material/KeyboardArrowUp';
+import Typography from '@mui/material/Typography';
+
+export default function Gallery() {
+  const [index, setIndex] = useState(0);
+  const [showMore, setShowMore] = useState(false);
+  const hasNext = index < goatList.length - 1;
+  const hasPrevious = index > 0;
+
+  function handleNextClick() {
+    if (hasNext) {
+      setIndex(index + 1);
+    } else {
+      setIndex(0);
+    }
+  }
+
+  function handleBackClick() {
+    if (hasPrevious) {
+      setIndex(index - 1);
+    } else {
+      setIndex(goatList.length - 1); 
+    }
+  }
+
+  function handleMoreClick() {
+    setShowMore(!showMore);
+  }
+
+  let sculpture = goatList[index];
+
+  return (
+    <Container maxWidth="sm">
+      <Box component="section" sx={{ p: 2, border: '1px dashed grey', position: 'relative' }}>
+        
+        <Typography variant="h4" gutterBottom>
+          NBA Legends
+        </Typography>  
+        <Typography variant="body1" color="textSecondary">
+          Yhuan Kyle D. David - C-PEITEL3
+        </Typography>
+
+        <Stack
+          spacing={2}
+          direction="row"
+          sx={{
+            marginBottom: 4, 
+            marginTop: 2,    
+          }}
+        >
+          <Button variant="contained" onClick={handleBackClick}>
+            Back
+          </Button>
+          <Button variant="contained" onClick={handleNextClick}>
+            Next
+          </Button>
+        </Stack>
+
+        <Box sx={{ position: 'relative' }}>
+          <img
+            src={sculpture.url}
+            alt={sculpture.alt}
+            style={{
+              width: '90%',  
+              maxWidth: '550px',  
+              height: 'auto',
+              borderRadius: '8px',
+              objectFit: 'cover',
+              marginBottom: 16,  
+              display: 'block',
+              marginLeft: 'auto',
+              marginRight: 'auto',
+            }}
+          />
+        </Box>
+
+        <Typography variant="h5" component="h2" sx={{ fontStyle: 'italic', mb: 1 }}>
+          {sculpture.name}
+        </Typography>
+
+        <Typography variant="body2" color="textSecondary" sx={{ mb: 2 }}>
+          ({index + 1} of {goatList.length})
+        </Typography>
+
+        <IconButton 
+          onClick={handleMoreClick} 
+          sx={{
+          marginLeft: 60,
+
+          }}
+        >
+          {showMore ? <KeyboardArrowUpIcon /> : <KeyboardArrowDownIcon />}
+        </IconButton>
+
+        {showMore && (
+          <Typography variant="body1" sx={{ mt: 1 }}>
+            {sculpture.description}
+          </Typography>
+        )}
+      </Box>
+    </Container>
+  );
+}
---
+++ src/data.tsx
@@ -0,0 +1,18 @@
+export const goatList = [{
+    name: 'Kevin Durant',
+    description: 'A nearly 7-foot scorer with guard-like skills, KD is one of the most unstoppable offensive forces in NBA history. With a deadly jumper, elite ball-handling, and the ability to score from anywhere, he makes getting buckets look effortless. His dominance has led to 2 NBA championships, 2 Finals MVPs, a league MVP, and 4 scoring titles, cementing him as one of the greatest pure scorers ever.',
+    url: 'https://s.yimg.com/ny/api/res/1.2/ti0SOoForNaOHAjOtgbrnQ--/YXBwaWQ9aGlnaGxhbmRlcjt3PTEyNDI7aD04Mjg-/https://media.zenfs.com/en/azcentral-the-arizona-republic/6ffe4f72769e2eb32eb3e3de02e8e70f',
+    alt: 'A photo of KD.'
+  },
+  {
+    name: 'Lebron James',
+    description: 'A once-in-a-lifetime talent, LeBron is the ultimate all-around player, combining elite size, skill, and basketball IQ. He can score at will, make pinpoint passes, dominate the boards, and defend multiple positions, making him a true do-it-all superstar. Over two decades, he has won 4 NBA championships, 4 MVPs, 4 Finals MVPs, and holds the title of the NBA’s all-time leading scorer, solidifying his place in the GOAT conversation.',
+    url: 'https://image-cdn.essentiallysports.com/wp-content/uploads/20200702112824/lebron-james-flexing.jpg?width=600',
+    alt: 'A photo of Lebron.'
+},
+  {
+    name: 'Stephen Curry',
+    description: 'The greatest shooter the game has ever seen, Steph revolutionized basketball with his deep-range threes, quick release, and unmatched off-ball movement. His ability to stretch defenses and handle the ball at an elite level changed how teams approach the game. His impact has led to 4 NBA championships, 2 MVPs, the only unanimous MVP in history, a Finals MVP, and the record for the most three-pointers ever made.',
+    url: 'https://s.hdnux.com/photos/01/35/02/25/24388988/3/rawImage.jpg',
+    alt: 'A photo of Curry.'
+}];
